fa7d1883e9453843149f58296e693d4ea7c648b1
lxykad <503215875@qq.com>
2016-11-08 15:57:10 +0800

    first commit

---
+++ app/src/main/java/test/lxy/com/demo/bean/FSbean.java
@@ -0,0 +1,23 @@
+package test.lxy.com.demo.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by lxy on 16/11/7.
+ */
+
+public class FSbean {
+
+    public String time;
+    public double open;
+    public double high;
+    public double low;
+    public double close;
+    public int volume;
+    public int balance;
+    public ArrayList<Integer> fundflow;
+    public ArrayList<Integer> BSPressure;
+
+
+}
